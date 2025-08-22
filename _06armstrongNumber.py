6f3feacefbc4890da9b6bd7e5d4e6f62e26069a3
devaSilverWolf <deva@silverforest.com>
2025-08-22 23:14:17 +0530

    tweak in armstrong num

--- _06armstrongNumber.py
+++ _06armstrongNumber.py
@@ -6,7 +6,7 @@ def checkArmStrongNumber(number:str):
         print("False Input I Repeat False Input...")
         exit(0)
 
-    if int(number) == sum(list(map(lambda x:x**3,list(map(int,list(number)))))):
+    if int(number) == sum(list(map(lambda x:x**len(number),list(map(int,list(number)))))):
         print(number," The Given is Armstrong Number")
     else:
         print(number, " The Given is not an Armstrong Number")
